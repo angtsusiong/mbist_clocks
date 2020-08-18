67cc7b63540e49a6a0aa8ac4981745fa8af5beba
dc1970 <doron.caspi@gmail.com>
2020-08-18 11:51:29 -0400

    Create hw_port

--- create_test.py
+++ create_test.py
@@ -3,4 +3,18 @@
 if __name__ == '__main__':
     print('PyCharm')
 
-    csv_file =
+    csv_file = 'mbist_clks.csv'
+    test_file = 'dft_mbist_clks.e'
+
+    csv_file = open(csv_file, 'r').read().splitlines()
+    outfile = open(test_file, 'w')
+
+    for line in csv_file:
+        clock_path = (line.split(',')[0])
+        clock_path = clock_path.replace('/', '.')
+        # print(clock_path)
+        outfile.write('def_hwp PIXCK_CLK of bit = "' + clock_path + '";\n')
+
+
+
+
